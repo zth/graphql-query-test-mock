15dd16ff7150560cd6d081bf564b2ace46eb9b10
Julien Nakache <julien@hioscar.com>
2019-01-30 08:50:01 -0500

    lint and clean up

--- src/handleErrors.js
+++ src/handleErrors.js
@@ -9,7 +9,11 @@ export function printNoMockFoundError(
   operationName: string,
   variables: Variables
 ): void {
-  const errorMessageArray = getNoMockFoundErrorMessageArray(queryMock, operationName, variables);
+  const errorMessageArray = getNoMockFoundErrorMessageArray(
+    queryMock,
+    operationName,
+    variables
+  );
   throw new Error(errorMessageArray.join(''));
 }
 
@@ -17,11 +21,11 @@ function getNoMockFoundErrorMessageArray(
   queryMock: QueryMock,
   operationName: string,
   variables: Variables
-) : Array<string> {
+): Array<string> {
   const mockedQueriesMessageArray = [
     `Could not find matching mock for operation "${operationName}" ` +
-    `with variables: ${JSON.stringify(variables)}\n` +
-    `Make sure you have mocked the query you are making.\n\n`
+      `with variables: ${JSON.stringify(variables)}\n` +
+      `Make sure you have mocked the query you are making.\n\n`
   ];
 
   if (!Object.entries(queryMock._queries).length) {
@@ -30,29 +34,33 @@ function getNoMockFoundErrorMessageArray(
   }
 
   if (queryMock._queries[operationName]) {
-    mockedQueriesMessageArray.push(`=== Currently mocked "${operationName}" queries ===\n`);
-    queryMock._queries[operationName].forEach(({queryMockConfig}) => {
-        mockedQueriesMessageArray.push(
-          `Query "variables": ${JSON.stringify(queryMockConfig.variables)}\n` +
+    mockedQueriesMessageArray.push(
+      `=== Currently mocked "${operationName}" queries ===\n`
+    );
+    queryMock._queries[operationName].forEach(({ queryMockConfig }) => {
+      mockedQueriesMessageArray.push(
+        `Query "variables": ${JSON.stringify(queryMockConfig.variables)}\n` +
           `Diff of "variables":\n` +
           `${printDiff(queryMockConfig.variables, variables)}\n\n`
-        );
+      );
     });
   }
 
   const otherMockedQueryNames = Object.keys(queryMock._queries).filter(
-    (queryMockName) => (operationName != queryMockName)
+    queryMockName => operationName !== queryMockName
   );
   if (!otherMockedQueryNames.length) {
     return mockedQueriesMessageArray;
   }
 
   mockedQueriesMessageArray.push(
-    `=== ${queryMock._queries[operationName] ? 'Other' : 'All'} mocked queries ===\n`
+    `=== ${
+      queryMock._queries[operationName] ? 'Other' : 'All'
+    } mocked queries ===\n`
   );
-  otherMockedQueryNames.forEach((queryName) => {
+  otherMockedQueryNames.forEach(queryName => {
     mockedQueriesMessageArray.push(`- "${queryName || 'unknown'}"\n`);
-  })
+  });
   mockedQueriesMessageArray.push(`\n`);
   return mockedQueriesMessageArray;
 }
@@ -64,7 +72,8 @@ export function printVariablesDoesNotMatchError(
   variables: Variables
 ): void {
   if (shouldMatchOnVariables) {
-    let errorStr = `Variables do not match for operation "${operationName}"`;
+    let errorStr = `Variables do not match for operation "${operationName ||
+      'unknown'}"`;
 
     if (queryMockConfig.matchVariables) {
       throw new Error(`${errorStr} due to custom "matchOnVariables" function`);
