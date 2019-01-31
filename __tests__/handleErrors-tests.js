ee0b6c27e5a72df83ca5d22098b48e41995f32d5
Julien Nakache <julien@hioscar.com>
2019-01-30 19:02:53 -0500

    lint file

--- __tests__/handleErrors-tests.js
+++ __tests__/handleErrors-tests.js
@@ -2,30 +2,29 @@
 import { fetchQuery } from '../__testUtils__/fetchQuery';
 import { queryMock } from '../__testUtils__/queryMock';
 
-
 describe('handleErrors', () => {
   describe('printNoMockFoundError', () => {
     it('should throw an error when there is no query name', async () => {
       try {
         const res = await fetchQuery({
-          text: 'query { id }',
+          text: 'query { id }'
         });
       } catch (error) {
         expect(error.message).toMatchSnapshot();
         return;
-      };
+      }
       throw new Error('No error was raised');
     });
 
     it('should throw an error when there is no mocked query', async () => {
       try {
         const res = await fetchQuery({
-          text: 'query TestQuery { id }',
+          text: 'query TestQuery { id }'
         });
       } catch (error) {
         expect(error.message).toMatchSnapshot();
         return;
-      };
+      }
       throw new Error('No error was raised');
     });
 
@@ -34,37 +33,8 @@ describe('handleErrors', () => {
         name: 'TestQuery',
         data: {},
         variables: {
-          variable1: 'VARIABLE_1',
-        },
-      });
-
-      try {
-        const res = await fetchQuery({
-          text: 'query TestQuery { id }',
-        });
-      } catch (error) {
-        expect(error.message).toMatchSnapshot();
-        return;
-      };
-      throw new Error('No error was raised');
-    });
-
-    it('should throw an error when there are multiple matching query names', async () => {
-      queryMock.mockQuery({
-        name: 'TestQuery',
-        data: {},
-        variables: {
-          variable1: 'VARIABLE_1',
-        },
-      });
-      queryMock.mockQuery({
-        name: 'TestQuery',
-        data: {
-          test: 'DATA 2',
-        },
-        variables: {
-          variable2: 'VARIABLE_2',
-        },
+          variable1: 'VARIABLE_1'
+        }
       });
 
       try {
@@ -74,14 +44,43 @@ describe('handleErrors', () => {
       } catch (error) {
         expect(error.message).toMatchSnapshot();
         return;
-      };
+      }
+      throw new Error('No error was raised');
+    });
+
+    it('should throw an error when there are multiple matching query names', async () => {
+      queryMock.mockQuery({
+        name: 'TestQuery',
+        data: {},
+        variables: {
+          variable1: 'VARIABLE_1'
+        }
+      });
+      queryMock.mockQuery({
+        name: 'TestQuery',
+        data: {
+          test: 'DATA 2'
+        },
+        variables: {
+          variable2: 'VARIABLE_2'
+        }
+      });
+
+      try {
+        const res = await fetchQuery({
+          text: 'query TestQuery { id }'
+        });
+      } catch (error) {
+        expect(error.message).toMatchSnapshot();
+        return;
+      }
       throw new Error('No error was raised');
     });
 
     it('should throw an error when there is 1 non-matching query names', async () => {
       queryMock.mockQuery({
         name: 'TestQuery1',
-        data: {},
+        data: {}
       });
 
       try {
@@ -91,24 +90,24 @@ describe('handleErrors', () => {
       } catch (error) {
         expect(error.message).toMatchSnapshot();
         return;
-      };
+      }
       throw new Error('No error was raised');
     });
 
     it('should throw an error when there are multiple non-matching query names', async () => {
       queryMock.mockQuery({
         name: 'TestQuery1',
-        data: {},
+        data: {}
       });
 
       queryMock.mockQuery({
         name: 'TestQuery2',
-        data: {},
+        data: {}
       });
 
       queryMock.mockQuery({
         name: '', // unknown
-        data: {},
+        data: {}
       });
 
       try {
@@ -118,7 +117,7 @@ describe('handleErrors', () => {
       } catch (error) {
         expect(error.message).toMatchSnapshot();
         return;
-      };
+      }
       throw new Error('No error was raised');
     });
 
@@ -127,12 +126,12 @@ describe('handleErrors', () => {
         name: 'TestQuery1',
         data: {},
         variables: {
-          variable1: 'VARIABLE_1',
-        },
+          variable1: 'VARIABLE_1'
+        }
       });
       queryMock.mockQuery({
         name: 'TestQuery2',
-        data: {},
+        data: {}
       });
 
       try {
@@ -142,7 +141,7 @@ describe('handleErrors', () => {
       } catch (error) {
         expect(error.message).toMatchSnapshot();
         return;
-      };
+      }
       throw new Error('No error was raised');
     });
   });
